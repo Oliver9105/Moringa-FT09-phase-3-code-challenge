8c2144af6566b2d57574dd8a1441007b08cf320b
Oliver cheruiyot <olivercher1000@gmail.com>
2025-01-09 01:21:18 +0300

    updated database connection

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,5 @@
 import sqlite3
 
-DATABASE_NAME = './database/magazine.db'
-
-def get_db_connection(db_path=None):
-    """Establish a connection to the SQLite database."""
-    # Use the provided database path or fall back to the default
-    db_path = db_path or DATABASE_NAME
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row  # Allows row access by column name
+def get_db_connection():
+    conn = sqlite3.connect('database/magazine.db')
     return conn
